1d888083d83ae197fc25434f3d9bcaabf3dc2e00
Chilletic <70214069+Chilletic@users.noreply.github.com>
2021-06-15 19:56:05 +0200

    Array vervollständigt

--- mp3_v3.ts
+++ mp3_v3.ts
@@ -159,8 +159,8 @@ namespace mp3_V3 {
     }
 
     function asciiConverter(): number{
-        let alphabetNumbers :number[] = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74];
-        let alphabetChars :string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        let alphabetNumbers :number[] = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+        let alphabetChars :string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
         let dirNameArray = dirName.split("");
 
